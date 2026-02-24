a201e9316cbaee51fefc796fe0421793cdfe0067
Efaz433 <efazsayeedahmed@gmail.com>
2026-02-24 23:22:11 +0600

    fourth commit

--- functionality.js
+++ functionality.js
@@ -5,17 +5,21 @@ const interviewJobsBtn = document.getElementById("interview-Jobs-btn");
 const rejectedJobsBtn = document.getElementById("rejected-Jobs-btn");
 const interviewCards = document.getElementById("interview-cards");
 const rejectedCards = document.getElementById("rejected-cards");
-console.log(rejectedCards.children.length)
+let totalInterview = document.getElementById("total-interview")
+let totalReject = document.getElementById("total-reject")
+let total = document.getElementById("total")
 
 interviewJobsBtn.addEventListener("click", () => {
     if (interviewCards.children.length === 0) {
         jobCardContainer.classList.add("hidden");
         rejectedCards.classList.add("hidden")
         emptySection.classList.remove("hidden")
+        total.innerText = interviewCards.children.length
     } else {
         jobCardContainer.classList.add("hidden");
         interviewCards.classList.remove("hidden")
         emptySection.classList.add("hidden")
+        total.innerText = interviewCards.children.length
     }
 
 })
@@ -24,10 +28,12 @@ rejectedJobsBtn.addEventListener("click", () => {
         jobCardContainer.classList.add("hidden");
         interviewCards.classList.add("hidden")
         emptySection.classList.remove("hidden")
+        total.innerText = rejectedCards.children.length
     } else {
         jobCardContainer.classList.add("hidden");
         rejectedCards.classList.remove("hidden")
         emptySection.classList.add("hidden")
+        total.innerText = rejectedCards.children.length
     }
 })
 allJobsBtn.addEventListener("click", () => {
@@ -35,5 +41,30 @@ allJobsBtn.addEventListener("click", () => {
     emptySection.classList.add("hidden");
     interviewCards.classList.add("hidden")
     rejectedCards.classList.add("hidden")
+    total.innerText = jobCardContainer.children.length
 })
+
+
+// adding cards to interview section
+const interview = document.querySelectorAll(".interview");
+interview.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let cloneDiv = btn.parentNode.parentNode.cloneNode(true);
+        interviewCards.appendChild(cloneDiv)
+        totalInterview.innerText = interviewCards.children.length;
+    });
+
+});
+
+// adding cards to reject section
+const reject = document.querySelectorAll(".reject");
+reject.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let cloneDiv = btn.parentNode.parentNode.cloneNode(true);
+        rejectedCards.appendChild(cloneDiv)
+        totalReject.innerText = rejectedCards.children.length;
+    });
+
+});
+
 
